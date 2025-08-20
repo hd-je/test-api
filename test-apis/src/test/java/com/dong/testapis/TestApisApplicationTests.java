3c10591aec78f84f8c6d27934e5c0bdb2bfc1eee
dong <jhd328@gmail.com>
2025-08-20 20:33:31 +0900

    first commit

---
+++ test-apis/src/test/java/com/dong/testapis/TestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.testapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
